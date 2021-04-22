8a2db3004ffd0ae9865d8b2c9a1594d503eff5f4
dninemfive <11084010+dninemfive@users.noreply.github.com>
2021-04-22 05:10:19 -0700

    center game

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
-    scene: [Menu, Play]
+    scene: [Menu, Play],
+    autoCenter: Phaser.CENTER_HORIZONTALLY
 };
 let game = new Phaser.Game(config);
 
