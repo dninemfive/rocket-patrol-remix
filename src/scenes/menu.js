ca08ae296c67e1ce85995f7653881ad1dd7f5077
dninemfive <11084010+dninemfive@users.noreply.github.com>
2021-04-28 05:43:13 -0700

    set number of ships in game

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -21,6 +21,7 @@ class Menu extends Phaser.Scene {
         } else {
             this.controlText.text = this.keyboardCtrlStr;
         }
+        this.numShipsText = this.add.text(game.config.width / 2, (borderUISize + borderPadding) * 4, "Press N to set the number of ships: " + numShips, textConfig).setOrigin(0.5);
         this.add.text(game.config.width / 2, game.config.height / 2 + borderUISize + borderPadding, "Press ← for Novice or → for Expert", menuConfig).setOrigin(0.5);
         //this.add.text(game.config.width / 2, game.config.height / 2, "✗", textConfig).setOrigin(0.5);
         //this.add.text(game.config.width / 2 + 40, game.config.height / 2, "✓", menuConfig).setOrigin(0.5);
@@ -28,6 +29,7 @@ class Menu extends Phaser.Scene {
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
         this.keyM = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.M);
+        this.keyN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.N);
     }    
 
     toggleMouse(){
@@ -37,6 +39,22 @@ class Menu extends Phaser.Scene {
         } else {
             this.controlText.text = this.keyboardCtrlStr;
         }
+    }
+
+    setNumShips(){
+        let result = "NaN";        
+        while(isNaN(result)){
+            result = prompt("Enter the number of ships");
+        }
+        let num = parseInt(result);
+        if(num < 1) {
+            num = 1;
+        }
+        if(num > 5){
+            num = 5;
+        }
+        numShips = num;
+        this.numShipsText.text = "Press N to set the number of ships: " + numShips;
     }
 
     update(){
@@ -59,6 +77,9 @@ class Menu extends Phaser.Scene {
         }
         if(Phaser.Input.Keyboard.JustDown(this.keyM)){
             this.toggleMouse();
+        }
+        if(Phaser.Input.Keyboard.JustDown(this.keyN)){
+            this.setNumShips();
         }
     }    
 }
